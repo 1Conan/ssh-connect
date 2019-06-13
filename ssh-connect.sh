355b7e79a862d854bbef525918fc2e6017a34e5e
1Conan <me@1conan.com>
2019-06-13 17:33:47 +0900

    Fix ssh-history

--- ssh-connect.sh
+++ ssh-connect.sh
@@ -11,9 +11,8 @@ fi
 source "$src"
 
 ssh-history() {
-  cat "$HISTFILE" | grep -E "^ssh\s" | sed -e 's/\s*$//' | sort | uniq -c | sort -nr | sed -e "s/^\s*[0-9]*\s//"
+  cat ${HISTFILE} | grep -E ":\ [0-9]+:[0-9]+;ssh\s" | sed -e 's/^:\ [0-9]*:[0-9]*;//' | sed -e 's/\s*$//' | sort | uniq -c | sort -nr | sed -e "s/^\s*[0-9]*\s//"}
 }
-
 ssh-connect() {
   local hist=$(ssh-history | tr '\n' '|')
   res=$(listbox -t "Connect:" -o "$hist" | tee /dev/tty | tail -n 1)
